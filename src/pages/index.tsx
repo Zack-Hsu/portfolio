8a41462d8ce161ca15d2064b02eac022ddf800c1
Zack-Hsu <zackxu841127@gmail.com>
2025-03-18 00:08:01 +0800

    變更 Team Taiwan 為首頁

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,12 @@
-"use client";
-
-import React, { useState } from "react";
-import {
-    Text,
-    Icon,
-    SmartImage,
-    Line,
-    Card,
-    Fade,
-    Column,
-    Row,
-    Grid,
-    Background
-} from "@/once-ui/components";
-
-import BaseTemplate from "@/elements/BaseTemplate/BaseTemplate";
-import HomeHero from "@/elements/Heros/HomeHero"
-import FlipReveal3D from "@/elements/AnimationEffects/FlipReveal3D";
+import FlipReveal3D from "@/elements/AnimationEffects/FlipReveal3D/FlipReveal3D";
 import PolishTemplate from "@/elements/PolishTemplate/PolishTemplate";
-import ProjectCard from "@/elements/Cards/ProjectCard/ProjectCard";
-export default function Home() {
-    const [iconIsActive, setIconIsActive] = useState(false)
+import { Background } from "@/once-ui/components";
+import TeamTaiwanHero from "@/elements/Heros/TeamTaiwanHero/TeamTaiwanHero";
+export default function () {
     return (
         <PolishTemplate>
             <Background
-                position="absolute"
+                position="relative"
                 gradient={{
                     display: true,
                     x: 100,
@@ -36,24 +18,9 @@ export default function Home() {
                     colorStart: "accent-background-strong",
                     colorEnd: "page-background",
                 }}
-            />
-            <Grid columns={1} position="relative" width="l">
-                <Grid columns={1} position="relative" width="l">
-                    <HomeHero />
-                    <Grid columns={1} width="s" align="center" marginTop="xl">
-                        <ProjectCard
-                            images={[
-                                { src: '/images/projects/prison/prison.png', alt: "Prison Main" },
-                                { src: '/images/projects/prison/p1.png', alt: "Prison Main" },
-                                { src: '/images/projects/prison/p2.png', alt: "Prison Main" },
-                            ]}
-                            title="國防部軍人監獄"
-                            description="這是一個 3D 虛擬導覽 專案，結合 Unity、krpano 以及 前端網頁技術，打造線上沉浸式的 軍事監獄歷史展示。使用者可透過 互動式 3D 模型 和 全景視角 瀏覽監獄內部環境，並查看相關的歷史資訊、影像與解說。"
-                            link="https://flying.myds.me/XindianMilitaryPrison/"
-                        />
-                    </Grid>
-                </Grid>
-            </Grid>
-        </PolishTemplate >
-    );
+            >
+                <TeamTaiwanHero />
+            </Background>
+        </PolishTemplate>
+    )
 }
