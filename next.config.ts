701e7618cf49289852552b2b5f49603e5b110db3
zack_hsu <zackxu841127@gmail.com>
2025-03-17 18:48:08 +0800

    修正next.confi.js

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   trailingSlash: true,
   output: "export",
-  basePath: '/', // 設定 GitHub Pages 的子目錄
+  basePath: '', // 設定 GitHub Pages 的子目錄
   assetPrefix: '/', // 確保靜態資源的路徑正確
   images: {
     unoptimized: true,
